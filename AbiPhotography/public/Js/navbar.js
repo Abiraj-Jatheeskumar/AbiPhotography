934bf058cd5048472f3e35f4441a636bb47c9cc6
Abiraj-Jatheeskumar <139776152+Abiraj-Jatheeskumar@users.noreply.github.com>
2024-06-05 09:47:00 +0530

    Add files via upload

---
+++ AbiPhotography/public/Js/navbar.js
@@ -0,0 +1,15 @@
+
+var navlinks = document.getElementById("navlinks");
+
+function shownav(){
+    navlinks.style.right = "0";
+    navlinks.style.display = "block";
+    
+}
+
+function hidenav(){
+    navlinks.style.right = "-200px";
+    navlinks.style.display = "none";
+    
+}
+
